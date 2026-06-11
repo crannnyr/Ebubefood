3c34034dd1104278f999d202b6948c552f05c2ad
crannnyr <grandswisssm@gmail.com>
2026-06-11 02:48:27 +0100

    Update OrdersTracker.tsx

--- src/components/profile/OrdersTracker.tsx
+++ src/components/profile/OrdersTracker.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
-  Calendar, ShoppingBag, CheckCircle, XCircle,
+  Calendar, CheckCircle, XCircle,
   ChevronRight, LayoutDashboard,
 } from 'lucide-react';
 import { useStore } from '@/store/useStore';
